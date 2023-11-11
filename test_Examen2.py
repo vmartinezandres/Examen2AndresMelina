d0f22c55ab21c2e7cb998841bfcc117370ecaa8f
lyna006 <pauporras@estudiantec.cr>
2023-11-11 14:14:41 -0600

    Pruebas unitarias completas

--- test_Examen2.py
+++ test_Examen2.py
@@ -11,5 +11,14 @@ class TestExamen(unittest.TestCase):
         objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])    
         self.assertEqual(objeto.DivisibleTempo(10), [1, 2, 5, 10])  
         self.assertEqual(objeto.DivisibleTempo(7), [1, 7])
+    def test_ObtieneMasBailable(self):
+        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(objeto.ObtieneMasBailable([0.8, 0.9, 0.7]), 0.9)  
+        self.assertEqual(objeto.ObtieneMasBailable([0.3, 0.1, 0.6]), 0.6)
+    def test_VerificaListaCanciones(self):
+        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]), True)  
+        self.assertEqual(objeto.VerificaListaCanciones([None]), False ) 
+       
 if __name__ == "__main__":
     unittest.main()
