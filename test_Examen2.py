79d209a76eb9a2d5ac2543286252a24ea6546a8e
lyna006 <pauporras@estudiantec.cr>
2023-11-11 14:40:05 -0600

    Prueba unitaria para Encuentra

--- test_Examen2.py
+++ test_Examen2.py
@@ -19,6 +19,10 @@ class TestExamen(unittest.TestCase):
         objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
         self.assertEqual(objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]), True)  
         self.assertEqual(objeto.VerificaListaCanciones([None]), False ) 
-       
+    def test_Encuentra(self):
+        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(objeto.Encuentra([1, 2, 3], 2), True)  
+        self.assertEqual(objeto.Encuentra([10, 8 ,2], 9), False ) 
+        
 if __name__ == "__main__":
     unittest.main()
